c345c3c3e282e1b9d9d487b7dcd0b267e4ca9d3c
Mauro Delmondes <derumondesu@gmail.com>
2021-07-19 09:22:19 -0400

    Definir Movimentos da Cobra

--- script.js
+++ script.js
@@ -2,23 +2,58 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+
 snake[0] = {
     x: 8 * box,
     y: 8 * box
-}
+};
+
+let direction = "right";
 
 
-function criarBG() {
+function criaBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobra() {
+function criaCobra() {
     for(i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarCobra();
+function iniciaJogo() {
+    criaBG();
+    criaCobra();
+
+    let snakeX = snake[0].x ;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") {
+        snakeX += box;
+    }
+
+    if(direction == "left") {
+        snakeX -= box;
+    }
+
+    if(direction == "up") {
+        snakeY -= box;
+    }
+
+    if(direction == "down") {
+        snakeY += box;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciaJogo, 100);
